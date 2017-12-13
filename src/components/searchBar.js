109d13ddfec982362b51085f8028637023928461
Josue Duran <josue.duran@globant.com>
2017-12-13 01:03:11 -0600

    Props

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,17 +3,20 @@ import React, {Component} from 'react';
 
 // {Component} = const Component = React.Component 
 // Xlass component, use when we need to add too logic to the component
+// Class component
 class SearchBar  extends Component {
 
   constructor(props){
     super(props);
-    this.state = {term: ''}; //Only class components have state obj
+    this.state = {term: 'Defult value'}; //Only class components have state obj
   }
 
 // Always define a render method
   render() {
     return <div>
-            <input onChange={event =>  this.setState({ term: event.target.value }) } />
+            <input 
+              value={this.state.term}
+              onChange={event =>  this.setState({ term: event.target.value }) } />
             Value: {this.state.term}
           </div>
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 // Components
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
 
-const API_YOUTUBE_KEY = 'AIzaSyDsFiWaI0OujvF7D4WlKYsRO9Oh_f70Eho';
+const API_YOUTUBE_KEY = 'AIzaSyDd27p4CsTb2bnC5A8kMtOeiCyrKijYSsA';
+
+
+// App component as class component
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+    YTSearch(
+      {
+        key: API_YOUTUBE_KEY,
+        term: 'surfboards'
+      },
+      (videos) => {
+        console.log('data', videos);
+        this.setState({videos});
+      }
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 //Create a new component that generate HTML
-const App =  () => {
+// Functional component, no use state
+/*const App =  () => {
   return (
     <div>
       <SearchBar />
     </div>
   );
-}
+}*/
+
+
 
 const container = document.querySelector('.container');
 //Insert component into the DOM
